211167c28e0766783208b1a82a195de68d297cd1
bhautik2304 <rathodbhautikrameshbhai@gmail.com>
2024-04-05 12:33:34 +0530

    all changes

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,12 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Photokrafft.com')
-<img src="https://basira.in/assets/logo.png" width="250" alt="Photokrafft.com">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Photokrafft.com')
+                <img src="https://basira.in/public/logo.png" width="250" alt="Photokrafft.com">
+            @else
+                {{ $slot }}
+            @endif
+        </a>
+    </td>
 </tr>
